80a3bc9ae99dace7c549c958f8c633371dec3b90
yrrho2 <yrrho2@naver.com>
2024-03-14 09:47:48 +0900

    init

---
+++ src/test/java/com/nhnacademy/store99/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.store99.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
