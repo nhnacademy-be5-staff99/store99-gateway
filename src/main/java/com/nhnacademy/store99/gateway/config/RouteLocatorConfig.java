4e49ef53d2de6da7698d7dc0cf01ce1df76b1aff
김승규 <rlatmdrb1996@gmail.com>
2024-04-02 15:14:58 +0900

    feat: gateway rewrite 적용
    Resolves: #11 https://github.com/nhnacademy-be5-staff99/store99-gateway/issues/11

--- src/main/java/com/nhnacademy/store99/gateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/store99/gateway/config/RouteLocatorConfig.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.store99.gateway.config;
 
-import com.nhnacademy.store99.gateway.property.RouteLocatorProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -10,19 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Configuration
 public class RouteLocatorConfig {
-    private final RouteLocatorProperties routeLocatorProperties;
-
     @Bean
     public RouteLocator store99Route(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("bookstore", r -> r.path(routeLocatorProperties.getBookstorePath())
-                        .uri(routeLocatorProperties.getBookstoreUri()))
-                .route("coupon", r -> r.path(routeLocatorProperties.getCouponPath())
-                        .uri(routeLocatorProperties.getCouponUri()))
-                .route("token-bookstore", r -> r.path(routeLocatorProperties.getTokenBookstorePath())
-                        .uri(routeLocatorProperties.getTokenBookstoreUri()))
-                .route("token-coupon", r -> r.path(routeLocatorProperties.getTokenCouponPath())
-                        .uri(routeLocatorProperties.getTokenCouponUri()))
+                .route("api-bookstore", r -> r.path("/api/bookstore/**")
+                        .filters(f -> f.rewritePath("api/bookstore/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://STORE99-BOOKSTORE-SERVICE"))
+                .route("api-coupon", r -> r.path("/api/coupon/**")
+                        .filters(f -> f.rewritePath("api/coupon/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://STORE99-COUPON-SERVICE"))
+                .route("open-bookstore", r -> r.path("/open/bookstore/**")
+                        .filters(f -> f.rewritePath("open/bookstore/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://STORE99-BOOKSTORE-SERVICE"))
+                .route("open-coupon", r -> r.path("/open/coupon/**")
+                        .filters(f -> f.rewritePath("open/coupon/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://STORE99-COUPON-SERVICE"))
                 .build();
     }
 }
